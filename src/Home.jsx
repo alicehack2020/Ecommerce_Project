e86896c9cd5afef7bd36db9a319082ec5878409e
vijay4211 <100846999+vijay4211@users.noreply.github.com>
2025-03-05 16:44:15 +0530

    navigation done and applied css on login page, registration page (#9)
    Co-authored-by: vijay4211 <vijayppandit2042.com>

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  let navigate = useNavigate();
+  console.log(navigate);
 
-export default Home
+  const productNavigator = () => {
+    navigate("/product");
+  };
+
+  const loginNagivator = () => {
+    navigate("/login");
+  };
+
+  const registrationNavigator = () => {
+    navigate("/registration");
+  };
+
+  return (
+    <div>
+      <div className="flex justify-end w-full border">
+        <div className="flex h-15 gap-3 md:gap-10  items-center px-2 md:px-5 cursor-pointer text-sm md:none">
+          <div onClick={productNavigator}>Product</div>
+          <div onClick={loginNagivator}>Login</div>
+          <div onClick={registrationNavigator}>Registration</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
