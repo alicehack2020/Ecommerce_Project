6d6ad355b752ef4b02959b3f4c8196ab6fd98eab
vijay4211 <vijayppandit2042.com>
2025-03-06 20:22:47 +0530

    create detail page

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Product from "./Product";
 
 import Registration from "./pages/Registration";
 import Login from "./pages/Login";
-
+import Detailpage from "./pages/Detailpage";
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/product" element={<Product />} />
           <Route path="/registration" element={<Registration />} />
-
+          <Route path="/detailpage" element={<Detailpage />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
